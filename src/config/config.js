a7089e599ebc75b496e2a53718e63b08f0b71a52
anderson ocana <anderson.ocana@ort.edu.ar>
2025-11-19 20:19:27 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,10 +8,10 @@ const {
     SUPABASE_URL,
     SUPABASE_API_KEY,
     DATABASE,
+    HOST
 } = process.env
 
 PORT = process.env.PORT || 8080
-HOST = process.env.HOST || "0.0.0.0"
 
 export const config = {
     SUPABASE_URL,
